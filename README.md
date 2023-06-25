a9b3a232c7739d6841b3edf1f0d53f87507ec5b2
Mr.Li <63112788+undercurre@users.noreply.github.com>
2023-06-25 19:32:58 +0800

    ✨feat(custom): 实现问答

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## typescript？
 
 这个脚手架使用 yeoman 进行开发,暂时没有发现它的触发器 yo 能使用 ts
+
+##
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,4 +1,9 @@
 var Generator = require('yeoman-generator')
+const figlet = require('figlet')
+const axios = require('axios')
+const { promisify } = require('util')
+const libconfig = require('../app/libconfig')
+const exec = promisify(require('child_process').exec)
 
 module.exports = class extends Generator {
   // The name `constructor` is important here
@@ -7,13 +12,175 @@ module.exports = class extends Generator {
     super(args, opts)
 
     // Next, add your custom code
-    this.option('babel') // This method adds support for a `--babel` flag
+    // this.option('babel') // This method adds support for a `--babel` flag
   }
-  method1() {
-    this.log('method 1 just ran')
+  async prompting() {
+    console.log(
+      '\r\n' +
+        figlet.textSync('Matrix', {
+          font: 'Ghost',
+          horizontalLayout: 'default',
+          verticalLayout: 'default',
+          width: 80,
+          whitespaceBreak: true,
+        })
+    )
+    const hello = await this.prompt([
+      {
+        type: 'list',
+        name: 'purpose',
+        message: 'Select your purpose',
+        choices: [
+          {
+            name: 'basic',
+            value: 'basic',
+            description: 'I need a basic template',
+          },
+          {
+            name: 'business',
+            value: 'business',
+            description: 'I need a business template',
+          },
+          {
+            name: 'tool',
+            value: 'tool',
+            description: 'I need a tool template',
+          },
+        ],
+      },
+    ])
+    let chose = null
+    if (hello.purpose === 'basic') {
+      const basic = await this.prompt([
+        {
+          type: 'checkbox',
+          name: 'carrier',
+          message: 'Select your carrier',
+          choices: [
+            {
+              name: 'web',
+              value: 'web',
+              description: 'I need a web template',
+            },
+            {
+              name: 'app',
+              value: 'app',
+              description: 'I need a app template',
+            },
+            {
+              name: 'desktop',
+              value: 'desktop',
+              description: 'I need a desktop template',
+            },
+            {
+              name: 'miniprogram',
+              value: 'miniprogram',
+              description: 'I need a miniprogram template',
+            },
+            {
+              name: 'weex',
+              value: 'weex',
+              description: 'I need a miniprogram template',
+            },
+            {
+              name: 'flutter',
+              value: 'flutter',
+              description: 'I need a flutter template',
+            },
+          ],
+        },
+      ])
+      chose = basic.basic
+    }
+    if (hello.purpose === 'business') {
+      const business = await this.prompt([
+        {
+          type: 'list',
+          name: 'purpose',
+          message: 'Select your purpose',
+          choices: [
+            {
+              name: 'questionnaire_outline',
+              value: 'questionnaire_outline',
+              description: 'A questionnaire with strapi outline',
+            },
+            {
+              name: 'questionnaire_online',
+              value: 'questionnaire_online',
+              description: 'A questionnaire with strapi online',
+            },
+            {
+              name: 'aromatherapy',
+              value: 'aromatherapy',
+              description: 'A car aromatherapy outline',
+            },
+            {
+              name: 'scale',
+              value: 'scale',
+              description: 'A body fat scale with weex in Meiju',
+            },
+          ],
+        },
+      ])
+      chose = business.business
+    }
+    if (hello.purpose === 'tool') {
+      const tool = await this.prompt([
+        {
+          type: 'list',
+          name: 'tool',
+          message: 'Select tool type',
+          choices: [
+            {
+              name: 'cli',
+              value: 'cli',
+              description: 'A template for cli',
+            },
+            {
+              name: 'component',
+              value: 'component',
+              description: 'A template for component library',
+            },
+          ],
+        },
+      ])
+      chose = tool.tool
+    }
+    const config = await this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname, // Default to current folder name
+      },
+    ])
+    this.answers = {
+      chose,
+      config,
+    }
   }
+  async writing() {
+    const { owner, repo, branch } = libconfig[this.answers.chose] // 从用户的答案中获取 owner、repo 和 branch
 
-  method2() {
-    this.log('method 2 just ran')
+    try {
+      const response = await axios.get(
+        `https://api.github.com/repos/${owner}/${repo}/zipball/${branch}`
+      )
+      const downloadUrl = response.data.zipball_url
+
+      const tmpFolder = '.tmp'
+      await exec(`git clone ${downloadUrl} ${tmpFolder}`)
+
+      this.fs.copy(
+        `${this.destinationPath(tmpFolder)}/`,
+        this.destinationPath()
+      )
+
+      await exec(`rm -rf ${tmpFolder}`)
+
+      this.log('Code downloaded successfully.')
+    } catch (error) {
+      this.log(`Failed to download code from GitHub: ${error.message}`)
+    }
   }
 }
